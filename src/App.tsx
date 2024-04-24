3172ff3622c5fcd28af375341c575e1267f68f18
Daniela <danielagp96@gmail.com>
2024-04-24 17:43:13 -0600

    BudgetForm working with state

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
       <header className="">
         <h1 className="bg-blue-600 text-white from-black text-center uppercase text-4xl font-bold p-10">Control de gastos</h1>
       </header>
-      <div className="max-w-4xl mx-auto bg-white rounded-md shadow-lg mt-10 p-10 flex justify-center">
+      <div className="max-w-4xl mx-auto bg-white rounded-md shadow-lg mt-10 p-10 ">
         <BudgetForm/>
       </div>
     </>
--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,8 +1,36 @@
+import { useState } from "react";
 
 function BudgetForm() {
+  const [budget, setBudget] = useState(0);
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setBudget(e.target.valueAsNumber);
+  }
   return (
-    <>BudgetForm</>
-  )
+    <form className="space-y-5">
+      <section className="flex flex-col space-y-5">
+        <label
+          htmlFor="budget"
+          className="text-blue-600 text-4xl from-black font-bold text-center"
+        >
+          Define tu presupuesto
+        </label>
+        <input
+          type="number"
+          id="budget"
+          name="budget"
+          placeholder="Cuanto tienes"
+          className="border-gray-200  border-solid border-2 w-full p-2"
+          value={budget}
+          onChange={handleChange}
+        />
+      </section>
+      <input
+        type="submit"
+        value="definir presupuesto"
+        className="bg-blue-600 hover:bg-blue-700 cursor-pointer text-white from-black font-bold p-2 uppercase text-base w-full"
+      />
+    </form>
+  );
 }
 
-export default BudgetForm
+export default BudgetForm;
