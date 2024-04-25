486ff88dbe0a94910f8270d5485efd01e4a89272
Daniela <danielagp96@gmail.com>
2024-04-24 21:45:22 -0600

    showing conditionally BudgetForm if submit is succesful hide it and show a new component BudgetTracker

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { useMemo } from "react";
 import BudgetForm from "./components/BudgetForm";
+import { useBudget } from "./hooks/useBudget";
+import BudgetTracker from "./components/BudgetTracker";
 
 function App() {
   //SECTION - 8. consumir la informacion compartida en mi caso el state y el dispatch que tiene mi context
  
   //SECTION - 10. consumir el context usando un customHook, ver commit anterior para entender por que 8. esta vacio
-  //FIXME - me equivoque aca era {state,dispatch} no []
-  //NOTE -  por ahora no necesito esto en app pues ya no necesito pasarlo por props, uso directo en BudgetForm component
-  //const {state,dispatch} = useBudget();
+
+
+  const {state} = useBudget();
+  
+  //SECTION - valida si el presupuesto ingresado en el input es valido, si si esconde el form y muestra otro nuevo componente
+  const isValidBudget = useMemo(()=>state.budget > 0 ,[state.budget])
 
   return (
     <>
@@ -14,7 +20,7 @@ function App() {
         <h1 className="bg-blue-600 text-white from-black text-center uppercase text-4xl font-bold p-10">Control de gastos</h1>
       </header>
       <div className="max-w-4xl mx-auto bg-white rounded-md shadow-lg mt-10 p-10 ">
-        <BudgetForm/>
+        {isValidBudget?<BudgetTracker/>:<BudgetForm/>}
       </div>
     </>
   );
